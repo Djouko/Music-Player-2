9fecd878160a7de18fbb8cecd376ce1f828ba259
hercule_socrate <destructeurkratos@gmail.com>
2022-07-20 17:40:08 +0100

    Music Player 2

---
+++ Music_Player2/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Music_Player2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.iconButton4 = new FontAwesome.Sharp.IconButton();
+            this.iconButton3 = new FontAwesome.Sharp.IconButton();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.icbtnfile = new FontAwesome.Sharp.IconButton();
+            this.icbtnmusic = new FontAwesome.Sharp.IconButton();
+            this.icbtnmedia = new FontAwesome.Sharp.IconButton();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.guna2ProgressBar1 = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.WindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(183, 569);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.iconButton4);
+            this.panel3.Controls.Add(this.iconButton3);
+            this.panel3.Controls.Add(this.iconButton2);
+            this.panel3.Controls.Add(this.iconButton1);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.icbtnmedia);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 132);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(183, 425);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.icbtnfile);
+            this.panel4.Controls.Add(this.icbtnmusic);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.ForeColor = System.Drawing.Color.Red;
+            this.panel4.Location = new System.Drawing.Point(0, 50);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(183, 100);
+            this.panel4.TabIndex = 1;
+            this.panel4.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.iconPictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(183, 132);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(79)))));
+            this.panel5.Controls.Add(this.guna2ProgressBar1);
+            this.panel5.Controls.Add(this.pictureBox1);
+            this.panel5.Controls.Add(this.WindowsMediaPlayer1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(183, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(617, 569);
+            this.panel5.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Music_Player2.Properties.Resources._2747630_christmas_music_musical_player_turntable_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(196, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 216);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // iconButton4
+            // 
+            this.iconButton4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.iconButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton4.ForeColor = System.Drawing.Color.Red;
+            this.iconButton4.IconChar = FontAwesome.Sharp.IconChar.SignOut;
+            this.iconButton4.IconColor = System.Drawing.Color.Red;
+            this.iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton4.Location = new System.Drawing.Point(0, 300);
+            this.iconButton4.Name = "iconButton4";
+            this.iconButton4.Size = new System.Drawing.Size(183, 50);
+            this.iconButton4.TabIndex = 5;
+            this.iconButton4.Text = "Log Out";
+            this.iconButton4.UseVisualStyleBackColor = true;
+            // 
+            // iconButton3
+            // 
+            this.iconButton3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.iconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton3.ForeColor = System.Drawing.Color.Red;
+            this.iconButton3.IconChar = FontAwesome.Sharp.IconChar.Question;
+            this.iconButton3.IconColor = System.Drawing.Color.Red;
+            this.iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton3.Location = new System.Drawing.Point(0, 250);
+            this.iconButton3.Name = "iconButton3";
+            this.iconButton3.Size = new System.Drawing.Size(183, 50);
+            this.iconButton3.TabIndex = 4;
+            this.iconButton3.Text = "Help";
+            this.iconButton3.UseVisualStyleBackColor = true;
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.ForeColor = System.Drawing.Color.Red;
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.Toolbox;
+            this.iconButton2.IconColor = System.Drawing.Color.Red;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton2.Location = new System.Drawing.Point(0, 200);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Size = new System.Drawing.Size(183, 50);
+            this.iconButton2.TabIndex = 3;
+            this.iconButton2.Text = "Tools";
+            this.iconButton2.UseVisualStyleBackColor = true;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.ForeColor = System.Drawing.Color.Red;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.ListDots;
+            this.iconButton1.IconColor = System.Drawing.Color.Red;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.Location = new System.Drawing.Point(0, 150);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(183, 50);
+            this.iconButton1.TabIndex = 2;
+            this.iconButton1.Text = "PlayList";
+            this.iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // icbtnfile
+            // 
+            this.icbtnfile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.icbtnfile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icbtnfile.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.icbtnfile.IconColor = System.Drawing.Color.Black;
+            this.icbtnfile.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.icbtnfile.Location = new System.Drawing.Point(0, 51);
+            this.icbtnfile.Name = "icbtnfile";
+            this.icbtnfile.Size = new System.Drawing.Size(183, 50);
+            this.icbtnfile.TabIndex = 2;
+            this.icbtnfile.Text = "Open File";
+            this.icbtnfile.UseVisualStyleBackColor = true;
+            this.icbtnfile.Click += new System.EventHandler(this.icbtnfile_Click);
+            // 
+            // icbtnmusic
+            // 
+            this.icbtnmusic.Dock = System.Windows.Forms.DockStyle.Top;
+            this.icbtnmusic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icbtnmusic.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.icbtnmusic.IconColor = System.Drawing.Color.Black;
+            this.icbtnmusic.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.icbtnmusic.Location = new System.Drawing.Point(0, 0);
+            this.icbtnmusic.Name = "icbtnmusic";
+            this.icbtnmusic.Size = new System.Drawing.Size(183, 51);
+            this.icbtnmusic.TabIndex = 1;
+            this.icbtnmusic.Text = "Open Music";
+            this.icbtnmusic.UseVisualStyleBackColor = true;
+            this.icbtnmusic.Click += new System.EventHandler(this.icbtnmusic_Click);
+            // 
+            // icbtnmedia
+            // 
+            this.icbtnmedia.Dock = System.Windows.Forms.DockStyle.Top;
+            this.icbtnmedia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icbtnmedia.ForeColor = System.Drawing.Color.Red;
+            this.icbtnmedia.IconChar = FontAwesome.Sharp.IconChar.Music;
+            this.icbtnmedia.IconColor = System.Drawing.Color.Red;
+            this.icbtnmedia.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.icbtnmedia.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.icbtnmedia.Location = new System.Drawing.Point(0, 0);
+            this.icbtnmedia.Name = "icbtnmedia";
+            this.icbtnmedia.Size = new System.Drawing.Size(183, 50);
+            this.icbtnmedia.TabIndex = 0;
+            this.icbtnmedia.Text = "Media";
+            this.icbtnmedia.UseVisualStyleBackColor = true;
+            this.icbtnmedia.Click += new System.EventHandler(this.icbtnmedia_Click);
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.iconPictureBox1.ForeColor = System.Drawing.Color.Red;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.HeadphonesAlt;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.Red;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 67;
+            this.iconPictureBox1.Location = new System.Drawing.Point(50, 34);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(71, 67);
+            this.iconPictureBox1.TabIndex = 1;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // guna2ProgressBar1
+            // 
+            this.guna2ProgressBar1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.guna2ProgressBar1.Location = new System.Drawing.Point(98, 507);
+            this.guna2ProgressBar1.Name = "guna2ProgressBar1";
+            this.guna2ProgressBar1.Size = new System.Drawing.Size(422, 12);
+            this.guna2ProgressBar1.TabIndex = 4;
+            this.guna2ProgressBar1.Text = "guna2ProgressBar1";
+            this.guna2ProgressBar1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            this.guna2ProgressBar1.ValueChanged += new System.EventHandler(this.guna2ProgressBar1_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // WindowsMediaPlayer1
+            // 
+            this.WindowsMediaPlayer1.Enabled = true;
+            this.WindowsMediaPlayer1.Location = new System.Drawing.Point(98, 432);
+            this.WindowsMediaPlayer1.Name = "WindowsMediaPlayer1";
+            this.WindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WindowsMediaPlayer1.OcxState")));
+            this.WindowsMediaPlayer1.Size = new System.Drawing.Size(422, 83);
+            this.WindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 569);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private FontAwesome.Sharp.IconButton icbtnfile;
+        private FontAwesome.Sharp.IconButton icbtnmusic;
+        private FontAwesome.Sharp.IconButton icbtnmedia;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton iconButton4;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private AxWMPLib.AxWindowsMediaPlayer WindowsMediaPlayer1;
+        private Guna.UI2.WinForms.Guna2ProgressBar guna2ProgressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Music_Player2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music_Player2
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+        String s;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void hide()
+        {
+            panel4.Visible = false;
+        }
+        private void presently()
+        {
+            if (panel4.Visible == false)
+            {
+                panel4.Visible = true;
+            }
+        }
+
+        private void icbtnmedia_Click(object sender, EventArgs e)
+        {
+            presently();
+        }
+
+        private void icbtnmusic_Click(object sender, EventArgs e)
+        {
+            hide();
+            OpenFileDialog dlg = new OpenFileDialog();
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                s = dlg.FileName;
+                runmp3();
+            }
+        }
+        private void runmp3()
+        {
+            WindowsMediaPlayer1.URL = s;
+            WindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void icbtnfile_Click(object sender, EventArgs e)
+        {
+            hide();
+        }
+
+        private void guna2ProgressBar1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (WindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                guna2ProgressBar1.Maximum = (int)WindowsMediaPlayer1.Ctlcontrols.currentItem.duration;
+                guna2ProgressBar1.Value = (int)WindowsMediaPlayer1.Ctlcontrols.currentPosition;
+
+            }
+        }
+    }
+}
